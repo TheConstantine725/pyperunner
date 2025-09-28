e0e1b172a1dfebba796138bf3216b9fe1d09eda0
Konstantinos Dimos <konstadimos7@gmail.com>
2025-09-28 20:01:39 +0300

    added schema and typemap orm object

--- src/pyperunner/models/source.py
+++ src/pyperunner/models/source.py
@@ -11,29 +11,44 @@ class Table(SourceBase):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     table_alias: Mapped[str] = mapped_column(unique=True)
-    is_incremental_load: Mapped[bool]
     raw_query: Mapped[str] 
+    dbapi_dialect: Mapped[str]
+    dbapi_driver: Mapped[str]
+    is_incrementally_loaded: Mapped[bool]
 
     merge_keys: Mapped[List["MergeKeys"]] = relationship("MergeKeys", back_populates="table", cascade="all, delete-orphan")
+    source_schema: Mapped[List["Schema"]] = relationship("Schema", back_populates="table", cascade= "all, delete-orphan")
 
-# class BindParams(SourceBase):
-#     __tablename__ = "src_bind_params"
+class Schema(SourceBase):
+    __tablename__ = "src_schema"
     
-#     table_id: Mapped[int] = mapped_column("source_table_id", ForeignKey("src_tables.id"), primary_key=True)
-#     table_bind_params_idx: Mapped[int] = mapped_column("bind_param_idx", index=True, primary_key=True)
-#     column_name: Mapped[str]
-
+    id: Mapped[int] = mapped_column(primary_key=True)
+    table_id: Mapped[int] = mapped_column(ForeignKey("src_tables.id"))
+    field_name: Mapped[str] = mapped_column(index=True)
+    type_code: Mapped[int]
+    display_size: Mapped[Optional[int]]
+    internal_size: Mapped[Optional[int]]
+    precision: Mapped[Optional[int]]
+    scale: Mapped[Optional[int]]
+    null_ok: Mapped[Optional[bool]]
+    
+    table: Mapped["Table"] = relationship("Table", back_populates="source_schema")
+    
 
 class MergeKeys(SourceBase):
     __tablename__ = "src_merge_keys"
     
-    merge_key_idx: Mapped[int] = mapped_column( index=True, nullable=False)
+    merge_key_idx: Mapped[int] = mapped_column( index = True, primary_key=True)
     column_name: Mapped[str]
     table_id: Mapped[int] = mapped_column(ForeignKey("src_tables.id"))
 
     table: Mapped["Table"] = relationship("Table", back_populates="merge_keys")
     
 
-
-
+class TypeMaps(SourceBase):
+    __tablename__ = "tm_type_maps"
     
+    id: Mapped[int] =  mapped_column(primary_key=True, index=True)
+    dbapi_driver: Mapped[str]
+    source_type_code: Mapped[int]
+    norm_dtype: Mapped[str]
